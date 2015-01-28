3ca0a72d745e58c5dc0afadb1530f2c167183570
Dan Thompson <danthompson41@gmail.com>
2015-01-28 12:38:45 -0500

    Working decoder

--- communicationdecoder.py
+++ communicationdecoder.py
@@ -5,9 +5,8 @@ import csv
 import re
 import matplotlib.pyplot as plt
 import numpy as np
-import collections
 
-
+#Quantize numbers into 0, 1, and 2 (1 is start bit, 2 is logic 1)
 def classify(number):
         if number <= 4:
             number = 0
@@ -17,6 +16,7 @@ def classify(number):
             number = 2
         return number
 
+#Creates moving average, in this case with a window of 5
 def movingaverage(lst,window):
     averagelist=[]
     for i, item in enumerate(lst):
@@ -34,24 +34,16 @@ def movingaverage(lst,window):
 list1 = []
 list2 = []
 list3 = []
+bitlist = []
 
 counter0 = 0
 counter1 = 0
 counter2 = 0
-flag0 = 0
-flag00 = 0
-flag1 = 0
-flag2 = 0
-flag22 = 0
 
-def resetflags():
-    flag0 = 0
-    flag00 = 0
-    flag2 = 0
-    flag22 = 0
+startflag = 0
 
 
-#test
+
 print("test")
 with open('hijack_dump.csv', 'rt') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
@@ -75,27 +67,39 @@ with open('hijack_dump.csv', 'rt') as csvfile:
             counter0 = counter0 + 1
             counter1 = 0
             counter2 = 0
-            if 12 == counter0:
+            if 10 == counter0:
                 print("------ZERO------")
-            elif 21 ==counter0:
-                print("------ZERO---")
+                if startflag == 1:
+                    bitlist.append(0)
+            if 30 == counter0:
+                print("------ZERO------")
+                counter0 = 10
+                if startflag == 1:
+                    bitlist.append(0)
+
         elif classifiednum == 1:
             counter0 = 0
             counter1 = counter1 + 1
             counter2 = 0
-            if counter1 == 25:
+            if counter1 == 15:
                 print("-----START BIT------")
                 counter1 = 0
+                startflag = 1
         elif classifiednum == 2:
-            if counter0 > 0 or counter1 >0:
-               resetflags()
             counter0 = 0
             counter1 = 0
             counter2 = counter2 + 1
-            if 12 == counter2:
+            if 8 == counter2:
                 print("------ONE------")
-            elif 24 == counter2:
-                print("------ONE---")
+                if startflag == 1:
+                    bitlist.append(1)
+            if 18 == counter2:
+                print("------ONE------")
+                counter2 = 8
+                if startflag == 1:
+                    bitlist.append(1)
+        if len(bitlist) == 8:
+            startflag = 0
         # print("C0:",counter0,"C1",counter1,"C2",counter2)
     # print(list3)
 
@@ -104,5 +108,6 @@ a = np.arange(0,len(list1),1)
 plt.plot(a, list1)
 plt.plot(a, list2)
 plt.plot(a, list3)
+print(bitlist)
 plt.show()
 time.sleep(1)
